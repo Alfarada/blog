b7264bc0d61a1e3080d6aaf181e909b3659150c7
Alfredo <alfaradayepez@gmail.com>
2020-08-17 18:49:43 -0500

    Configurando la conexion a la  base de datos y configuracion de la migracion Category

--- database/migrations/2020_08_17_233049_create_categories_table.php
+++ database/migrations/2020_08_17_233049_create_categories_table.php
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCategoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 64);
+            $table->string('slug', 128)->unique();
+            $table->mediumText('body')->nullable();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('categories');
