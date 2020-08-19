03dab0ad296b11d0cba405f6bc127154a7c8c365
Alfredo <alfaradayepez@gmail.com>
2020-08-18 22:18:32 -0500

    incluyendo paquete de autenticacion con laravel iu y personalizando rutas,controladores y vistas.

---
+++ app/Http/Controllers/Web/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    public function blog()
+    {
+        $posts = Post::orderBy('id', 'DESC')
+            ->where('status','PUBLISHED')
+            ->paginate(3);
+
+        return view('web.posts', compact('posts'));
+    }
+}
---
+++ resources/views/web/posts.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container d-flex justify-content-center">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Lista de Artículos</h1>
+            @foreach($posts as $post)
+                <div class="card mb-3">
+                    @if($post->file)
+                        <img src="{{$post->file}}" 
+                            class="img-responsive card-img-top"
+                            alt="{{ $post->name }}">
+                    @endif
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $post->name }}</h4>
+                        <p class="card-text">
+                            {{ $post->excerpt }}
+                        </p>
+                        <a href="#"
+                           class="d-flex justify-content-end">Leer más</a>
+                    </div>
+                </div>
+            @endforeach
+            {{ $posts->render() }}
+        </div>
+    </div>
+@endsection
